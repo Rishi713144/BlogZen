af8748ad079c430d85d1e0a9059f502f2614b3b1
Rishi <116449371+Rishi713144@users.noreply.github.com>
2025-12-04 17:12:09 +0530

    initial_commit

---
+++ common/dist/index.d.ts
@@ -0,0 +1,24 @@
+import z from 'zod';
+export declare const signUpInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+    name: z.ZodOptional<z.ZodString>;
+}, z.core.$strip>;
+export declare const signInInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, z.core.$strip>;
+export declare const createBlogInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+}, z.core.$strip>;
+export declare const updateBlogInput: z.ZodObject<{
+    title: z.ZodOptional<z.ZodString>;
+    content: z.ZodOptional<z.ZodString>;
+    id: z.ZodString;
+}, z.core.$strip>;
+export type SignUpInput = z.infer<typeof signUpInput>;
+export type SignInInput = z.infer<typeof signInInput>;
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
+//# sourceMappingURL=index.d.ts.map
