8486a9cdcc85b221cfd2531037de52c6e0651486
Rishi <116449371+Rishi713144@users.noreply.github.com>
2026-02-06 19:45:55 +0530

    feat: enhance backend API handling; add OPTIONS method and update Vercel configuration for routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
 import { userRouter } from './routes/user';
 import { blogRouter } from './routes/blog';
 import { cors } from 'hono/cors'
@@ -13,5 +14,12 @@ app.use('/*', cors())
 app.get('/', (c) => c.text('Hello Hono!'))
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const DELETE = handle(app);
+export const PATCH = handle(app);
+export const OPTIONS = handle(app);
 
 export default app
